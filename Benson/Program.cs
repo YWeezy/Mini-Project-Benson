2b70b5bdf380a268af09a97951832d5976e22ce7
GREED <pratikadhikari10@gmail.com>
2024-02-14 17:37:49 +0100

    made text rendering slower in console the base text speed is 40 to make it slower increase the number

--- Benson/Program.cs
+++ Benson/Program.cs
@@ -7,32 +7,42 @@ class Program
         World.PopulateQuests();
         World.PopulateLocations();
 
-        Console.WriteLine("Game World Initialized");
-
-        Location startingLocation = World.LocationByID(World.LOCATION_ID_HOME);
-        Console.WriteLine($"Welcome to {startingLocation.Name}.");
-        Console.WriteLine(startingLocation.Description);
-        Console.WriteLine("Do you want to move to the townsquare? (Yes/No)");
-
-        if (Console.ReadLine().ToUpper() == "YES")
+        Print("Game World Initialized", 60);
+        Console.WriteLine("Type 'Start' to begin the game or type 'Quit' twice to exit the game.");
+        if (Console.ReadLine().ToUpper() == "START")
         {
-            MoveToLocation(World.LOCATION_ID_TOWN_SQUARE);
+            Location startingLocation = World.LocationByID(World.LOCATION_ID_HOME);
+            Print($"Welcome {startingLocation.Name}.", 60);
+            Print(startingLocation.Description, 50);
+            Print("Do you want to move to the townsquare? (Yes/No)", 60);
+
+            if (Console.ReadLine().ToUpper() == "YES")
+            {
+                MoveToLocation(World.LOCATION_ID_TOWN_SQUARE);
+            }
+            else
+            {
+                Print("just say yes! What's wrong with you?",50);
+                Console.ReadLine();
+            }
+            Location newLocation = World.LocationByID(World.LOCATION_ID_TOWN_SQUARE);
+            Print($"You are now in {newLocation.Name}.", 50);
+            Print(newLocation.Description, 60);
+
+            DisplayAvaibleQuests(newLocation);
+
+        }
+        else if (Console.ReadLine().ToUpper() == "QUIT")
+        {
+            Print("Goodbye", 60);
         }
         else
         {
-            Console.WriteLine("just say yes! What's wrong with you?");
-            Console.ReadLine();
+            Print("Invalid command.",70);
         }
 
 
-        Location newLocation = World.LocationByID(World.LOCATION_ID_TOWN_SQUARE);
-        Console.WriteLine($"You are now in {newLocation.Name}.");
-        Console.WriteLine(newLocation.Description);
 
-        DisplayAvaibleQuests(newLocation);
-
-
-        Console.ReadLine();
     }
     static void MoveToLocation(int locationID)
     {
@@ -48,6 +58,14 @@ class Program
             {
                 Console.WriteLine($"- {quest.Name}: {quest.Description}");
             }
+        }
+    }
+    public static void Print(string text, int speed) // making speed a higher number will slow the text more. 
+    {
+        foreach (char c in text)
+        {
+            Console.Write(c);
+            System.Threading.Thread.Sleep(speed);
         }
     }
 }
