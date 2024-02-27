8e51668e9316b1f31452c211bb239b03ec56c036
Ach <74924664+Acharab@users.noreply.github.com>
2024-02-27 14:54:29 +0100

    removed confirmation for attack

--- Benson/Battle.cs
+++ Benson/Battle.cs
@@ -6,24 +6,17 @@ public class Battle
         do
         {
             Console.WriteLine($"You started a Quest. The moster is{monster.Name}");
-            Console.WriteLine("Do you want to attack? (Y/N)");
-            string choice = Console.ReadLine().Trim().ToUpper();
-            if (choice == "Y")
-            {
-                if (turn % 2 == 0)
-                {
-                    monster.Health -= weapon.Damage;
-                }
 
-                else
-                {
-                    player.HP -= monster.Damage;
-                }
+            if (turn % 2 == 0)
+            {
+                monster.Health -= weapon.Damage;
             }
+
             else
             {
-                break;
+                player.HP -= monster.Damage;
             }
+
             Console.WriteLine(turn % 2 == 0 ? $"The moster did {weapon.Damage} damage. The moster has {monster.Health} HP" : $"You did {monster.Damage} damage. You have {monster.Health} HP");
 
         } while (monster.Health <= 0 || player.HP <= 0);
